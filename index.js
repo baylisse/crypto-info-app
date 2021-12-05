4c6d039bfa06afc15ebe4324149efe21886a0736
Elizabeth (Betsy) Bayliss <76703615+baylisse@users.noreply.github.com>
2021-12-05 10:08:18 -0800

    tsc compile

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 const priceAPIURL = "/GetPrice/:currencyCode";
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
